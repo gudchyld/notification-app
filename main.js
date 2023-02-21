22df9b614a16c57d0fb6c9afe2741d3b5d66b2af
Valarie Oyieke <oyiekeav@gmail.com>
2023-02-21 10:49:13 +0300

    branch update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-console.log('Hello World!');
 
-let main = document.getElementById('main')
+// let main = document.getElementById('main')
 let markRead = document.querySelector('.mark-read')
 let unreadMsg = document.querySelectorAll(".bg-grey")
 let count = unreadMsg.length;
@@ -8,17 +7,22 @@ let notifyNum = document.querySelector('.notify-num')
 notifyNum.textContent = `${count}`;
 
 
-main.addEventListener('click', (e) => {
+
+document.addEventListener('click', (e) => {
   if (e.target.classList.contains('bg-grey')) {
      e.target.classList.remove('bg-grey');
      renderCount();
   }
 });
 
+
+
 markRead.addEventListener('click',() => {
   unreadMsg.forEach(item => item.classList.remove('bg-grey'));
   renderCount();
 })
+
+
 
 function renderCount(){
   let unreadMsg = document.querySelectorAll(".bg-grey")
@@ -28,6 +32,5 @@ function renderCount(){
 }
 
 
-
-console.log(count)
+// console.log(count)
 
