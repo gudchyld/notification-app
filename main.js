123c69eef56655916c4eb476cd4f80860b25d349
goodchild66@gmail.com <goodchild66@gmail.com>
2023-03-10 17:29:11 +0100

    comment on background removal

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
 
-// let main = document.getElementById('main')
 let markRead = document.querySelector('.mark-read')
 let unreadMsg = document.querySelectorAll(".bg-dark")
 let count = unreadMsg.length;
 let notifyNum = document.querySelector('.notify-num')
 notifyNum.textContent = `${count}`;
 
-
-
 document.addEventListener('click', (e) => {
+  //when any element close to the element with class name 'bg-dark' is clicked, return to the element with class name 'bg-dark' and remove 'bg-dark' from its classlist
   
      e.target.closest('.bg-dark').classList.remove('bg-dark');
-     
-     
      renderCount();
   })
-
-
-
 
 markRead.addEventListener('click',() => {
   unreadMsg.forEach(item => item.classList.remove('bg-dark'));
   renderCount();
 })
-
-
 
 function renderCount(){
   let unreadMsg = document.querySelectorAll(".bg-dark")
@@ -34,5 +25,4 @@ function renderCount(){
 }
 
 
-// console.log(count)
 
