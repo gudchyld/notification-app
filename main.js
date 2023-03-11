8cc282897fa55fa896d01e5433db341944d498df
Elendu <goodchild66@gmail.com>
2023-03-11 14:03:47 +0100

    red dot removal works on clicking specific regions

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 
 let markRead = document.querySelector('.mark-read')
 let unreadMsg = document.querySelectorAll(".bg-dark")
+let redDot = document.querySelectorAll('.small')
+
+
 let count = unreadMsg.length;
 let notifyNum = document.querySelector('.notify-num')
 notifyNum.textContent = `${count}`;
 
 document.addEventListener('click', (e) => {
   //when any element close to the element with class name 'bg-dark' is clicked, return to the element with class name 'bg-dark' and remove 'bg-dark' from its classlist
-  
+
+     if(e.target.parentElement.dataset.check){
      e.target.closest('.bg-dark').classList.remove('bg-dark');
+     document.getElementById(e.target.parentElement.dataset.check).style.display = 'none'
+    }
+     
      renderCount();
   })
 
